28b29f8384dbd9e6f722925576bdbed67eba251a
FeoHeo <nguyenduong8bc1@gmail.com>
2025-05-18 17:15:54 +0930

    saving before a2

--- numbers2.c
+++ numbers2.c
@@ -2,6 +2,7 @@
 // id: a1876928
 // Description: The pprogram will print out of place node
 // Note: The array used for testing is right below this line. Modify as you see fit to test the program
+int ARR[4] = {5,3,6,8}; // sample array for testing
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,7 +17,7 @@ int getTargetP(int curr_rank , int max_rank , int direction) {  // A function th
     } else {
         target_rank = (curr_rank + max_rank - 1) % max_rank;
     }
-    
+
     return target_rank;
 }
 
@@ -27,7 +28,7 @@ int edgeCase(int curr_rank , int max_rank) {   // Resolve edgecase at end and be
     } else if(curr_rank == 0) {
         return -1;
     }
-    
+
     return 0;
 }
 
@@ -36,24 +37,26 @@ void endProcess(int curr_rank) {
     MPI_Finalize();
 }
 
-int main(int *argc , char **argv[]) {
+int main(void) {
     int p_rank;
     int comm_sz;
-    
-    
-    MPI_Init(NULL,&argv);
+    int out_order_count = 0;
+    int count = 0;
+
+    int out_of_order_check = 1;
+    int order_check = 0;
+
+    MPI_Init(NULL,NULL);
     MPI_Comm_rank(MPI_COMM_WORLD , &p_rank);
     MPI_Comm_size(MPI_COMM_WORLD , &comm_sz);   // If run with 4 cores then size would be 4
-    
-    // int ARR[comm_sz] = {5,3,6,8}; // sample array for testing
-   
-    int node_data = int(argv[p_rank]);
-    
+
+    int node_data = ARR[p_rank];
+
     int target_R = getTargetP(p_rank , comm_sz , 1);
     int target_L = getTargetP(p_rank , comm_sz , 0);
     int data_L;
     int data_R;
-    
+
 
     // Send to right
     MPI_Send(&node_data , 1 , MPI_INT , target_R , 0 , MPI_COMM_WORLD);
@@ -70,19 +73,43 @@ int main(int *argc , char **argv[]) {
     int edge = edgeCase(p_rank , comm_sz);
     if(node_data > data_L && edge == 1) {
         printf("Process %d is fine\n" , p_rank);
+        MPI_Send(&order_check , 1 , MPI_INT , 0 , 2 , MPI_COMM_WORLD);
 
     } else if(node_data < data_R && edge == -1) {
         printf("Process %d is fine\n" , p_rank);
 
     } else if(edge) {
-        printf("Process %d has at least on number out of order\n" , p_rank);
+        printf("Process %d has at least one number out of order\n" , p_rank);
+        if(p_rank == 0) {
+            count++;
+        } else if(edge) {
+            MPI_Send(&out_of_order_check , 1 , MPI_INT , 0 , 2 , MPI_COMM_WORLD);
+        }
     }
 
+    // Check out of order
     if((node_data < data_L || node_data > data_R) && edge == 0) {
-        printf("Process %d has at least on number out of order\n" , p_rank);
+        printf("Process %d has at least one number out of order\n" , p_rank);
+        MPI_Send(&out_of_order_check , 1 , MPI_INT , 0 , 2 , MPI_COMM_WORLD);
 
     } else if(edge == 0) {
         printf("Process %d is fine\n" , p_rank);
+        MPI_Send(&order_check , 1 , MPI_INT , 0 , 2 , MPI_COMM_WORLD);
+    }
+
+
+    int count_buff;
+
+    if(p_rank == 0) {
+        for(int i=1 ; i<comm_sz ; i++) {
+            MPI_Recv(&count_buff , 1 , MPI_INT , i , 2 , MPI_COMM_WORLD , MPI_STATUS_IGNORE);
+            // printf("Received: %d\n" , count_buff);
+            if(count_buff) {
+                count++;
+            }
+        }
+
+        printf("The number of processes holding an out-of-order number is %d\n" , count);
     }
 
 
